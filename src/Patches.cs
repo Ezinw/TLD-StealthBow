2bd885c9f6f084d8cf570cf589e369983e25340c
Ezinw <112014367+Ezinw@users.noreply.github.com>
2024-03-22 13:33:54 -0300

    Update Patches.cs

--- src/Patches.cs
+++ src/Patches.cs
@@ -4,7 +4,7 @@ using Il2Cpp;
 namespace StealthBow
 {
     [HarmonyPatch(typeof(SkillsManager), "Awake")]
-    internal class StealthBow
+    internal class StealthBowSetting
     {
 
         public static void Postfix(SkillsManager __instance)
@@ -12,6 +12,29 @@ namespace StealthBow
             __instance.m_Skill_Archery.m_LevelWhereCanFireFromCrouch = Settings.settings.StealthBow;
         }
 
+    }
+
+    [HarmonyPatch(typeof(Skill_Archery), nameof(Skill_Archery.GetTierBenefits))]
+    public class StealthBow
+    {
+        static void Postfix(ref string __result, Skill_Archery __instance)
+        {
+            SkillTiers currentTier = (SkillTiers)__instance.GetCurrentTierNumber();
+
+            if (currentTier == SkillTiers.Beginner && Settings.settings.StealthBow == 1 ||
+                currentTier == SkillTiers.Novice && Settings.settings.StealthBow == 2 ||
+                currentTier == SkillTiers.Novice && Settings.settings.StealthBow == 1 ||
+                currentTier == SkillTiers.Skilled && Settings.settings.StealthBow == 3 ||
+                currentTier == SkillTiers.Skilled && Settings.settings.StealthBow == 2 ||
+                currentTier == SkillTiers.Skilled && Settings.settings.StealthBow == 1 ||
+                currentTier == SkillTiers.Expert && Settings.settings.StealthBow == 4 ||
+                currentTier == SkillTiers.Expert && Settings.settings.StealthBow == 3 ||
+                currentTier == SkillTiers.Expert && Settings.settings.StealthBow == 2 ||
+                currentTier == SkillTiers.Expert && Settings.settings.StealthBow == 1)
+            { __result += "\nCan fire bow when crouched"; }
+
+        }
+    
     }
 
 }
